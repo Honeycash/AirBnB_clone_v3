d9ee953bf578ddae3b081d87a916a749762baa4d
honeycash <endurancechery@gmail.com>
2024-05-09 07:09:09 -0700

    Api Restful Project

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,27 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """get:
+        retrieve an object from the file storage by clas and id. """
+
+        if cls and id:
+            if cls in classes.values() and isinstance(id, str):
+                all_objects = self.all(cls)
+                for key, value in all_objects.items():
+                    # if key.split('.')[1] == id:
+                    if value.get('id') == id:
+                        return value
+        return
+
+    def count(self, cls=None):
+        """ count the number of objects in storage matching the givenclass"""
+        if not cls:
+            inst_of_all_cls = self.all()
+            return len(inst_of_all_cls)
+        if cls in classes.values():
+            all_inst_of_prov_cls = self.all(cls)
+            return len(all_inst_of_prov_cls)
+        if cls not in classes.values():
+            return
